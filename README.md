c8c6a2493fffded4cb629333acdfb4769d01bc1e
filicaliva <41651332+filicaliva@users.noreply.github.com>
2020-06-10 11:12:03 +0200

    readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Note Timer
+> A simple app note with recording time.
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Link](#link)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+Add more general information about project. What the purpose of the project is? Motivation?
+
+## Screenshots
+![Example screenshot](./img/screenshot.png)
+![Example screenshot](./img/screenshot_02.png)
+
+## Technologies
+* HTML CSS JS 
+
+## Link
+[Note Timer]( https://filicaliva.github.io/work_timer/)
+
+## Status
+Project is: _finished_
+
+## Contact
+Created by [@filicaliva](https://www.linkedin.com/in/vfilimonchuk/) - feel free to contact me!
--- core.js
+++ core.js
@@ -2,87 +2,87 @@
 
 function saveTextAsFile() {
   const timertext = document.getElementById("timer").innerHTML,
-        textToWrite = document.getElementById('timerForm__text').value +'\n'+ timertext,
-        textFileAsBlob = new Blob([ textToWrite ], { type: 'application/msword' }),
-        fileNameToSaveAs = document.querySelector('[name = timerForm__title]').value + ' - ' + document.getElementById('timer').textContent + '.doc';
-        const downloadLink = document.createElement("a");
-        downloadLink.download = fileNameToSaveAs;
-        downloadLink.innerHTML = "Download File";
-        location.reload();
-    
-    if (window.webkitURL != null) {
-      // Chrome allows the link to be clicked without actually adding it to the DOM.
-      downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-    } else {
-      // Firefox requires the link to be added to the DOM before it can be clicked.
-      downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-      downloadLink.onclick = destroyClickedElement;
-      downloadLink.style.display = "none";
-      document.body.appendChild(downloadLink);
-    }
-  
-    downloadLink.click();
-  }
-  
-  function destroyClickedElement(event) {
-    // remove the link from the DOM
-    document.body.removeChild(event.target);
+    textToWrite = document.getElementById('timerForm__text').value + '\n' + timertext,
+    textFileAsBlob = new Blob([textToWrite], { type: 'application/msword' }),
+    fileNameToSaveAs = document.querySelector('[name = timerForm__title]').value + ' - ' + document.getElementById('timer').textContent + '.doc';
+  const downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  location.reload();
+
+  if (window.webkitURL != null) {
+    // Chrome allows the link to be clicked without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
   }
 
-  //  Timer
+  downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+  // remove the link from the DOM
+  document.body.removeChild(event.target);
+}
+
+//  Timer
 let t = 0, h = 0;
 var keepGoing = true;
 
 function myLoop() {
-  let minutes = h, 
-  seconds = t;
+  let minutes = h,
+    seconds = t;
 
-    if(t >= 59) {
-      h+=Math.ceil(t / 59);
-      t = -1;
-    } 
-    document.getElementById("timer").innerHTML = 
-    (minutes >= 10 ? minutes : '0' + minutes) 
-    + ":" + 
+  if (t >= 59) {
+    h += Math.ceil(t / 59);
+    t = -1;
+  }
+  document.getElementById("timer").innerHTML =
+    (minutes >= 10 ? minutes : '0' + minutes)
+    + ":" +
     (seconds >= 10 ? seconds : '0' + seconds);
-    t++;
+  t++;
 
-    if(keepGoing) {
-        setTimeout(myLoop, 1000);
-    }
+  if (keepGoing) {
+    setTimeout(myLoop, 1000);
+  }
 }
 
 function startLoop() {
-    keepGoing = true;
-    myLoop();
-    e.stopImmediatePropagation();
+  keepGoing = true;
+  myLoop();
+  e.stopImmediatePropagation();
 }
 
 function stopLoop() {
-    keepGoing = false;
+  keepGoing = false;
 }
 
 
 //animation circle
 let circle = document.querySelector('.circle'),
-    screen_two = document.querySelector('.two'),
-    timer = document.getElementById('timer');
-circle.addEventListener('click', function() {
-    this.style = 'animation: move 2s ease 1;'
-    setTimeout(function() {
-        circle.remove;
-        screen_two.style = 'animation: opacits 1s ease-in 1; display: block;';
-        circle.style = 'display: none;';
-        startLoop();
-      }, 1000);
+  screen_two = document.querySelector('.two'),
+  timer = document.getElementById('timer');
+circle.addEventListener('click', function () {
+  this.style = 'animation: move 2s ease 1;'
+  setTimeout(function () {
+    circle.remove;
+    screen_two.style = 'animation: opacits 1s ease-in 1; display: block;';
+    circle.style = 'display: none;';
+    startLoop();
+  }, 1000);
 });
 
-timer.addEventListener('mouseout', startLoop);
-timer.addEventListener('mouseover', stopLoop);
+timer.addEventListener('dblclick', startLoop);
+timer.addEventListener('click', stopLoop);
 
 const button = document.querySelector('#timerForm__svg');
-  button.addEventListener('click', saveTextAsFile); 
-  
+button.addEventListener('click', saveTextAsFile);
+
 
 
 
